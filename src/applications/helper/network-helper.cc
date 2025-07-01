438bacb251a4b8a7f9bd51462bea9ce1c09260a7
ZDQCDB <3238107011@qq.com>
2025-07-01 19:30:55 +0800

    Update network-helper.cc

--- src/applications/helper/network-helper.cc
+++ src/applications/helper/network-helper.cc
@@ -21,7 +21,7 @@ namespace ns3 {
         ApplicationContainer apps;
         int j = 0;
 
-        int leader_id = (rand() % m_nodeNo);        //主节点随机数选取
+        int leader_id = (rand() % m_nodeNo);
         
         for (NodeContainer::Iterator i = c.Begin (); i != c.End (); i++)
         {
@@ -33,13 +33,13 @@ namespace ns3 {
             if (j == leader_id) {                   //选中标记为主节点
                 app->is_leader = 1;
             } else {
-                app->is_leader = 0;                 //否则就是普通节点
+                app->is_leader = 0;
             }
 
-            app->leader_id= leader_id;              //设置主节点ID
-            app->view_number = 1;                   //初始化试图编号
-            app->client_id = -1;                    //客户端ID默认为-1
-            app->sec_num = 0;                       //安全编号初始化为0
+            app->leader_id= leader_id;
+            app->view_number = 1;
+            app->client_id = -1;
+            app->sec_num = 0;
             app->m_peersAddresses = m_nodesConnectionsIps[nodeId];      //获取节点的连接IP地址
             (*i)->AddApplication (app);             //将用于程序添加到节点上
             apps.Add (app);                         //将应用程序加入到应用容器
