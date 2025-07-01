4bd95e2d212ffc3bf2b99761c51d1c9e548cdb6a
ZDQCDB <3238107011@qq.com>
2025-07-01 19:22:40 +0800

    Update POS.h

--- POS/POS.h
+++ POS/POS.h
@@ -9,7 +9,7 @@
 #include "ns3/address.h"
 #include "ns3/boolean.h"
 #include "ns3/socket.h"
-#include "ns3/nstime.h" // 添加Time类定义
+#include "ns3/nstime.h"
 #include <map>
 #include <vector>
 #include <array>
@@ -20,7 +20,6 @@ class Address;
 class Socket;
 class Packet;
 
-// 将PBFT共识阶段枚举修改为POS共识阶段枚举
 enum POSPhase {
     STAKE_ANNOUNCE,    // 0 权益公布阶段
     BLOCK_PROPOSE,     // 1 区块提议阶段
@@ -69,33 +68,30 @@ class NodeApp : public Application
     Time m_blockInterval;                              // 出块间隔
     uint32_t m_maxRounds;                              // 最大共识轮次
     std::vector<Block> m_blockchain;                   // 本地区块链
-    std::map<Ipv4Address, uint32_t> messageCount;      // 消息计数(用于DoS检测)
+    std::map<Ipv4Address, uint32_t> messageCount;
     bool m_isValidator;                                // 当前轮次是否为验证者
     
     int             N;                                  // 节点总数
-    int             is_leader;                          // 是否为领导者(权益最高的节点)
+    int             is_leader;
     int             sec_num;                            // 交易序列号
     uint32_t        round_number;                       // 当前轮次
 
-    // 为兼容network-helper.cc添加的成员变量
-    int             leader_id;                          // 领导者ID (兼容原PBFT代码)
-    int             client_id;                          // 客户端ID (兼容原PBFT代码)
-    int             view_number;                        // 视图编号 (兼容原PBFT代码)
+    int             leader_id;
+    int             client_id;
+    int             view_number;
 
     double          m_reputation = 100.0;               // 节点信誉度
 
-    // 性能统计相关
-    Time m_roundStartTime;                              // 当前轮次开始时间
-    Time m_roundEndTime;                                // 当前轮次结束时间
-    Time m_totalTime;                                   // 所有轮次总时间
-    Time m_latencyStartTime;                            // 交易延迟开始时间
-    Time m_latencyEndTime;                              // 交易延迟结束时间
-    uint32_t m_successfulTxCount;                       // 成功交易数
-    uint32_t m_totalTxCount;                            // 总交易数
-    std::map<uint32_t, Time> m_txStartTimes;            // 交易开始时间记录
-    std::map<uint32_t, Time> m_txEndTimes;              // 交易结束时间记录
-    double m_avgLatency;                                // 平均交易时延
-
+    Time m_roundStartTime;
+    Time m_roundEndTime;
+    Time m_totalTime;
+    Time m_latencyStartTime;
+    Time m_latencyEndTime;
+    uint32_t m_successfulTxCount;
+    uint32_t m_totalTxCount;
+    std::map<uint32_t, Time> m_txStartTimes;
+    std::map<uint32_t, Time> m_txEndTimes;
+    double m_avgLatency;
     // 添加密钥相关成员变量
     std::array<unsigned char, 32> m_secretKey;          // 节点私钥
     std::array<unsigned char, 32> m_publicKey;          // 节点公钥
@@ -111,7 +107,6 @@ class NodeApp : public Application
 
     void SendTXWithDelay(uint8_t data[], int size, double delay);
 
-    // POS共识相关方法
     void InitializePos(uint32_t maxRounds, Time blockInterval);
     void StartPosConsensus();
     void RunConsensusRound(uint32_t round);
@@ -124,7 +119,6 @@ class NodeApp : public Application
     void HandleStakeMessage(const std::string& msg);
     //void CalculateMetrics();
 
-    // TPS和时延计算
     void SubmitTransaction(uint32_t transactionId);
     void ConfirmTransaction(uint32_t transactionId);
     double CalculateTPS();
